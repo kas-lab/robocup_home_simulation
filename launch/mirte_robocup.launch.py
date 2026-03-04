56931adcdc1fa78bf9d0986e95238cd941c201c1
Alex Gabriel <A.Gabriel@tudelft.nl>
2026-03-04 16:45:50 +0000

    templating files so we can use xacro to adjust to ignition and post-ignition

--- launch/mirte_robocup.launch.py
+++ launch/mirte_robocup.launch.py
@@ -1,8 +1,9 @@
+import os
+import xacro
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import AnyLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
-import os
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
@@ -12,11 +13,11 @@ def generate_launch_description():
     world_path = PathJoinSubstitution([
                 FindPackageShare('robocup_home_simulation'),
                 'worlds',
-                'robocup_world.sdf'])
+                'robocup_world.sdf.xacro'])
 
     gz_launch_file = os.path.join(get_package_share_directory("mirte_gazebo"),
                                   "launch",
-                                  "mirte_simulation_fortress.launch.py")
+                                  "mirte_simulation.launch.py")
 
     gz_launch = IncludeLaunchDescription(
         AnyLaunchDescriptionSource(gz_launch_file),
